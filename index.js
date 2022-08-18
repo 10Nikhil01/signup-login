dd3d03471f2a48159ddd5c34af231d4ee0c126ef
NIKHIL KUMAR <108527011+10Nikhil01@users.noreply.github.com>
2022-08-18 16:15:30 +0530

    modified

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+//Get the button
+var myButton = document.getElementById("myBtn");
+                
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
 const encryptData = {
   'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q',
   'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
